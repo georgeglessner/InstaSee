36fcd5b407d582155db3e4f5a0aaf9bb3e72e33d
georgeglessner <g.glessner@outlook.com>
2020-05-06 11:17:48 -0700

    Remove popup, url sanitization

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
-// Url of Instagram user
-let url = window.location.href;
+// Regex for urls
+let urlPattern = /https:\/\/www.instagram.com\/[^\/]*\//g;
+let imgPattern = /https:\/\/www.instagram.com\/[^\/]+\/p\/[^\/]+\//g;
+let imgPathPattern = /\/p\/[^\/]+\//g;
 
+// Profile url
+let url = window.location.href.match(urlPattern);
+
+// Listen for click
 document.addEventListener('click', function(event) {
     // initial target
     target = event.target;
@@ -8,9 +14,35 @@ document.addEventListener('click', function(event) {
     // Loop through targets until we find the link of image page
     while (target) {
       if (target.parentNode instanceof HTMLAnchorElement) {
-        window.open(url + target.parentNode.getAttribute('href').substring(1));
+        let imgPath = target.parentNode.getAttribute('href');
+        let imgMatch = imgPath.match(imgPattern);
+        let imgPathMatch = imgPath.match(imgPathPattern);
+
+        if(imgMatch && imgMatch.length > 0){
+          window.open(url[0] + imgMatch[0]);
+        } else if(imgPathMatch && imgPathMatch.length > 0) {
+          window.open(url[0] + imgPathMatch[0].substring(1));
+        }
+        removePopup();
         break;
       }
+      
       target = target.parentNode;
     }
 }, true);
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/* 
+  Remove popup from profile page
+*/
+async function removePopup(){
+  await sleep(1000);
+  document
+    .querySelectorAll('[role="presentation"]')
+    .forEach(function (element) {
+      element.remove()
+    })
+}
